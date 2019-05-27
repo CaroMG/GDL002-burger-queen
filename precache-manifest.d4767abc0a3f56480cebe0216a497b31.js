d7ea4fdbf70e67688750b4c48cee2647a2babd33
Carolina Manzano <CarolinaManzano@99.200-57-1.bestelclientes.com.mx>
2019-05-27 12:30:25 -0500

    Updates

---
+++ precache-manifest.d4767abc0a3f56480cebe0216a497b31.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f40b3dfbae937a313a306974290288d9",
+    "url": "/CaroMG/GDL002-burger-queen/index.html"
+  },
+  {
+    "revision": "a64bcb000a05e89c854b",
+    "url": "/CaroMG/GDL002-burger-queen/static/css/main.1b74fc3d.chunk.css"
+  },
+  {
+    "revision": "0cb64a077c4af20da17c",
+    "url": "/CaroMG/GDL002-burger-queen/static/js/2.425d73d9.chunk.js"
+  },
+  {
+    "revision": "a64bcb000a05e89c854b",
+    "url": "/CaroMG/GDL002-burger-queen/static/js/main.dc349e38.chunk.js"
+  },
+  {
+    "revision": "4964e3e50fae981e1e38",
+    "url": "/CaroMG/GDL002-burger-queen/static/js/runtime~main.e0929e80.js"
+  }
+]);
